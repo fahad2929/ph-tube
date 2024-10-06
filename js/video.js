036cb7b60fba65b94efd1c20759f1de63a7fc310
MD.ZISHANURL FAHAD <mdzishanurlfahad22@gmail.com>
2024-10-07 00:47:52 +0600

    added videos

--- js/video.js
+++ js/video.js
@@ -1,17 +1,51 @@
 // 1. Fetch, Load and show catagories on html 
 
 
-// create loadcatagories
+// create loadCatagories
 const loadCategories = () => {
       console.log("loadcatagories created")
-
     //   fetch the data
-
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error));
+};
+// create loadVideos
+const loadVideos = () => {
+    console.log("loadCatagories created")
+  //   fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+  .then((res) => res.json())
+  .then((data) => displayVideos(data.videos ))
+  .catch((error) => console.log(error));
 }
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("videos")
+    videos.forEach(video => {
+    console.log(video)
+    const card = document.createElement("div");
+    card.classList = "card card-compact"
+    card.innerHTML = `
+    <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `
+    videoContainer.append(card);
+    })
+
+
+};
+
 
 
 // create DisplayCatagories
@@ -35,3 +69,4 @@ const displayCategories = (categories) => {
 
 
 loadCategories()
+loadVideos()
